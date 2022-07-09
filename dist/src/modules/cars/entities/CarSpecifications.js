ec0a7ff62b87afe6849b4bfa5499fc5de5098028
Welton Nogueira <welton.nogueira.dev@gmail.com>
2022-07-09 15:12:34 -0300

    Delete dist directory

--- dist/src/modules/cars/entities/CarSpecifications.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CarsSpecifications = void 0;
-class CarsSpecifications {
-}
-exports.CarsSpecifications = CarsSpecifications;
